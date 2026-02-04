7067bbb216cc754908a4cbacf8491e7237757276
loosche <bradfordjamesloos@yahoo.com>
2026-02-04 14:33:04 -0800

    Use the correct name for the NAV layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -42,7 +42,7 @@ U_NP,              U_NP,              U_NA,              U_NA,              &kp 
 // K: DOWN - > UP
 // L: UP -> RIGHT
 // ;: RIGHT -> CAPS
-#define MIRYOKU_NAV \
+#define MIRYOKU_LAYER_NAV \
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &kp LEFT ,         &kp DOWN,          &kp UP,            &kp RIGHT,         &u_caps_word,      \
 U_NA,              &kp RALT,          &u_to_U_NUM,       &u_to_U_NAV,       U_NA,              &kp INS,           &kp HOME,          &kp PG_DN,         &kp PG_UP,         &kp END,           \
